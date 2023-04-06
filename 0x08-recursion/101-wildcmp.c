44a8cc962177dff445813f9d00e655b78e9d33c6
Wilkyster <wwinnom@gmail.com>
2023-04-05 18:32:41 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -17,10 +17,10 @@ return (wildcmp(s1, s2 + 1));
 }
 return (*s2 == '\0');
 }
+
 if (*s2 == '*')
 {
 return (wildcmp(s1 + 1, s2) ||
-}
 wildcmp(s1, s2 + 1));
 }
 else if (*s1 == *s2)
@@ -29,3 +29,4 @@ return (wildcmp(s1 + 1, s2 + 1));
 }
 return (0);
 }
+
